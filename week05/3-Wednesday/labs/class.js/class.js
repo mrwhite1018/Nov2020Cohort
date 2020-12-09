cb2e8e163cfc3ee37259a2d0dbdf6c12420ffc65
Cedael White <84crose@gmail.com>
2020-12-09 10:37:58 -0500

    stash

---
+++ week05/3-Wednesday/labs/class.js/class.js
@@ -0,0 +1,25 @@
+
+
+/*
+- 52 images  arrays objects
+    -array of objects
+    -{[]},{},{}]
+    -suit, value, imageURL
+    -2D.png, 3D, 4H
+deck: array of objects, with 52 elements
+-shuffle - provided
+- pop()
+-use pop objects, place (dom manipulation) the images on your screen
+-dealer array, player array
+    -cards pushed to these arrays
+- hit me (event listner)
+    -pop card from deck 
+    -push card to player hand (array)
+    -recalculate
+-stand (event listner)
+    -process starts for dealer
+    -check the value 
+
+- 2 buttons (hit, stand)
+
+*/
